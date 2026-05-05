d7d71d35681165881e30327d4472ac008d48d979
jubayer50 <aujubayer1@gmail.com>
2026-05-05 18:58:54 +0600

    banner image added

--- src/Components/HomePage/Banner.jsx
+++ src/Components/HomePage/Banner.jsx
@@ -4,6 +4,7 @@ import { useSpring, animated, easings } from "@react-spring/web";
 import Image from "next/image";
 import Link from "next/link";
 import { IoArrowDownOutline } from "react-icons/io5";
+import ImageJubayer from "@/assets/Images/jubayer.png";
 
 const Banner = () => {
   const springs = useSpring({
@@ -18,64 +19,91 @@ const Banner = () => {
     },
   });
 
+  const availableHighLight = useSpring({
+    from: { scale: 0.6, opacity: 100 },
+    to: { scale: 0.9, opacity: 50 },
+    loop: { reverse: true },
+    config: {
+      duration: 700,
+    },
+  });
+
   return (
-    <div className="py-18 md:py-25 text-center px-2">
-      <div className="">
-        <div>
-          <div className="flex items-center justify-center">
-            <h2 className="text-3xl md:text-[40px] font-bold">
-              Hello, I'm <span className="text-[#05F2F2]">Jubayer</span>{" "}
+    <div className="py-18 md:py-25 md:text-center px-2 max-w-310 mx-auto">
+      <div className="flex flex-col-reverse md:flex-row justify-center md:justify-between items-center gap-10">
+        <div className="text-center md:text-left">
+          <div>
+            <div className="flex items-center justify-center md:justify-start">
+              <h2 className="text-3xl md:text-[40px] font-bold">
+                Hello, I'm <span className="text-[#05F2F2]">Jubayer</span>{" "}
+              </h2>
+
+              <Image
+                src={
+                  "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHhydG1scTJveGdyeDJhNnRxemxhb3hrbHVnMzNod3p0N3llczhrNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/ykZS4KGnpdlY2f7XHg/giphy.gif"
+                }
+                alt="Hi Gif"
+                width={60}
+                height={60}
+              ></Image>
+            </div>
+
+            <h2 className="text-2xl md:text-[32px] font-medium text-[#7588A3]">
+              I built a highly scalable website{" "}
             </h2>
 
-            <Image
-              src={
-                "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHhydG1scTJveGdyeDJhNnRxemxhb3hrbHVnMzNod3p0N3llczhrNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/ykZS4KGnpdlY2f7XHg/giphy.gif"
-              }
-              alt="Hi Gif"
-              width={60}
-              height={60}
-            ></Image>
+            <p className="text-[#7588A3] max-w-160 mx-auto mt-5">
+              I am a passionate Frontend Web Developer skilled in JavaScript and
+              modern frameworks like React and Next.js. I build highly scalable
+              responsive, user-friendly interfaces with a focus on clean and
+              maintainable code.
+            </p>
           </div>
 
-          <h2 className="text-2xl md:text-[32px] font-medium text-[#7588A3]">
-            I built a highly scalable website{" "}
-          </h2>
+          <div className="flex items-center justify-center md:justify-start gap-3 mt-6 md:mt-10">
+            <a
+              href=""
+              download={""}
+              className="btn bg-[#05F2F2] border-none shadow-none"
+            >
+              Download Resume
+            </a>
 
-          <p className="text-[#7588A3] max-w-187.5 mx-auto mt-5">
-            I am a passionate Frontend Web Developer skilled in JavaScript and
-            modern frameworks like React and Next.js. I build highly scalable
-            responsive, user-friendly interfaces with a focus on clean and
-            maintainable code.
-          </p>
+            <animated.div style={springs}>
+              <IoArrowDownOutline className="text-[#7588A3] text-[22px]" />
+            </animated.div>
+
+            <Link href={"/projects"}>
+              <button className="btn bg-[#05F2F2] border-none shadow-none">
+                View Project
+              </button>
+            </Link>
+          </div>
         </div>
 
-        <div className="flex items-center justify-center gap-3 mt-6 md:mt-10">
-          <a
-            href=""
-            download={""}
-            className="btn bg-[#05F2F2] border-none shadow-none"
-          >
-            Download Resume
-          </a>
-
-          <animated.div style={springs}>
-            <IoArrowDownOutline className="text-[#7588A3] text-[22px]" />
-          </animated.div>
-
-          <Link href={"/projects"}>
-            <button className="btn bg-[#05F2F2] border-none shadow-none">
-              View Project
-            </button>
-          </Link>
+        <div className="relative w-70 h-70 md:w-95 md:h-95">
+          <Image
+            src={ImageJubayer}
+            alt="Jubayer"
+            width={600}
+            height={600}
+            className="rounded-full w-full h-full absolute object-cover border-3 border-[#05F2F250] z-20"
+          ></Image>
+          <div className="w-40 h-40 bg-[#05F2F2] rounded-full absolute top-[50%] left-[50%] z-5  blur-3xl opacity-40 translate-y-[-50%] translate-x-[-50%]"></div>
         </div>
       </div>
 
-      <div className="flex justify-center mt-6">
+      {/* 
+      //avilable now button
+      <div className="flex justify-center mt-5 md:mt-7">
         <button className="bg-[#0367A630] backdrop-blur-sm rounded-full border border-[#05F2F250] text-white text-[14px] px-3 py-1 flex items-center gap-2">
-          <div className="w-2 h-2 bg-[#05F2F2] rounded-full"></div>
+          <animated.div
+            className="w-2 h-2 bg-[#05F2F2] rounded-full"
+            style={availableHighLight}
+          ></animated.div>
           Available Now
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
