93bc279a0ee9dac32ec1f832cc55a65d6be96890
jubayer50 <aujubayer1@gmail.com>
2026-05-10 03:00:46 +0600

    project section one project added

---
+++ src/Components/Projects/Projects.jsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
+import Tiles from "@/assets/Images/tiles.jpg";
+
+const Projects = () => {
+  return (
+    <div className="max-w-310 mx-auto">
+      <div>
+        <h2 className="font-bold text-2xl md:text-2xl text-[#05F2F2]">
+          Projects
+        </h2>
+        <p className="font-medium text-lg text-[#7588A3] mt-2">
+          Explore some of my featured projects showcasing scalable, responsive,
+          and modern web development using React.js and Next.js
+        </p>
+
+        <div className="mt-10">
+          <div className="grid grid-cols-3 gap-5">
+            <div className="space-y-4 group bg-[#011b30] overflow-hidden rounded-lg border border-[#0367A640] hover:border-[#05F2F290] transition-all duration-400">
+              <div>
+                <Image
+                  src={Tiles}
+                  alt="Tiles Gallery"
+                  width={800}
+                  height={800}
+                  className="w-full rounded-md object-cover transition-transform duration-400 ease-in-out group-hover:scale-104"
+                ></Image>
+              </div>
+
+              <div className="p-5 space-y-4">
+                <div className="space-y-4">
+                  <h2 className="text-xl font-semibold text-[#05F2F2]">
+                    Tiles Gallery
+                  </h2>
+                  <p className="text-[#7588A3]">
+                    Tiles Gallery is a secure web application featuring Better
+                    Auth integration for Google and email/password login,
+                    protecting personalized user profiles and exclusive product
+                    data.
+                  </p>
+                </div>
+
+                <div className="flex items-center gap-4">
+                  <Link
+                    href={"https://github.com/jubayer50/Tiles-Gallery"}
+                    target="_blank"
+                  >
+                    <button className="btn text-[#021525] bg-[#05F2F2] shadow-none border-none">
+                      <FaGithub className="text-lg" /> Repository
+                    </button>
+                  </Link>
+
+                  <Link
+                    href={"https://tiles-gallery-jubayer-50.vercel.app/"}
+                    target="_blank"
+                  >
+                    <button className="btn text-[#021525] bg-[#05F2F2] shadow-none border-none">
+                      <FaExternalLinkAlt /> Live Project
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import About from "@/Components/About/About";
 import Banner from "@/Components/HomePage/Banner";
 import SkillsAndTechnology from "@/Components/HomePage/SkillsAndTechnology/SkillsAndTechnology";
+import Projects from "@/Components/Projects/Projects";
 import React from "react";
 
 const HomePage = () => {
@@ -11,6 +12,8 @@ const HomePage = () => {
       <About></About>
 
       <SkillsAndTechnology></SkillsAndTechnology>
+
+      <Projects></Projects>
     </div>
   );
 };
