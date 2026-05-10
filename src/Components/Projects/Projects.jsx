dc0dd4b902b8e46270a5f49be4a0d9ea4e96db73
jubayer50 <aujubayer1@gmail.com>
2026-05-10 15:34:31 +0600

    project page added

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
 
         <div className="mt-10">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-            <div className="space-y-4 group bg-[#011b30] overflow-hidden rounded-lg border border-[#0367A640] hover:border-[#05F2F290] transition-all duration-400">
+            <div className="space-y-4 group bg-[#011b30] overflow-hidden rounded-lg border border-[#0367A640] hover:border-[#05F2F290] transition-all duration-400 flex flex-col h-full">
               <div>
                 <Image
                   src={Tiles}
@@ -28,7 +28,7 @@ const Projects = () => {
                 ></Image>
               </div>
 
-              <div className="p-5 space-y-4">
+              <div className="p-5 space-y-4 flex flex-col justify-between flex-1">
                 <div className="space-y-4">
                   <h2 className="text-xl font-semibold text-[#05F2F2]">
                     Tiles Gallery
@@ -38,6 +38,97 @@ const Projects = () => {
                     Auth integration for Google and email/password login,
                     protecting personalized user profiles and exclusive product
                     data.
+                  </p>
+                </div>
+
+                <div className="flex items-center gap-4 ">
+                  <Link
+                    href={"https://github.com/jubayer50/Tiles-Gallery"}
+                    target="_blank"
+                  >
+                    <button className="btn text-[#021525] bg-[#05F2F2] shadow-none border-none">
+                      <FaGithub className="text-lg" /> Repository
+                    </button>
+                  </Link>
+
+                  <Link
+                    href={"https://tiles-gallery-jubayer-50.vercel.app/"}
+                    target="_blank"
+                  >
+                    <button className="btn text-[#021525] bg-[#05F2F2] shadow-none border-none">
+                      <FaExternalLinkAlt /> Live Project
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="space-y-4 group bg-[#011b30] overflow-hidden rounded-lg border border-[#0367A640] hover:border-[#05F2F290] transition-all duration-400 flex flex-col h-full">
+              <div>
+                <Image
+                  src={Tiles}
+                  alt="Tiles Gallery"
+                  width={800}
+                  height={800}
+                  className="w-full rounded-md object-cover transition-transform duration-400 ease-in-out group-hover:scale-104"
+                ></Image>
+              </div>
+
+              <div className="p-5 space-y-4 flex flex-col flex-1 justify-between">
+                <div className="space-y-4">
+                  <h2 className="text-xl font-semibold text-[#05F2F2]">
+                    Keen Keeper
+                  </h2>
+                  <p className="text-[#7588A3]">
+                    A React.js communication app where users can call, video
+                    call, and text friends while tracking all interactions in
+                    real time through timelines and visual stats charts.
+                  </p>
+                </div>
+
+                <div className="flex items-center gap-4">
+                  <Link
+                    href={"https://github.com/jubayer50/Tiles-Gallery"}
+                    target="_blank"
+                  >
+                    <button className="btn text-[#021525] bg-[#05F2F2] shadow-none border-none">
+                      <FaGithub className="text-lg" /> Repository
+                    </button>
+                  </Link>
+
+                  <Link
+                    href={"https://tiles-gallery-jubayer-50.vercel.app/"}
+                    target="_blank"
+                  >
+                    <button className="btn text-[#021525] bg-[#05F2F2] shadow-none border-none">
+                      <FaExternalLinkAlt /> Live Project
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="space-y-4 group bg-[#011b30] overflow-hidden rounded-lg border border-[#0367A640] hover:border-[#05F2F290] transition-all duration-400 flex flex-col h-full">
+              <div>
+                <Image
+                  src={Tiles}
+                  alt="Tiles Gallery"
+                  width={800}
+                  height={800}
+                  className="w-full rounded-md object-cover transition-transform duration-400 ease-in-out group-hover:scale-104"
+                ></Image>
+              </div>
+
+              <div className="p-5 space-y-4 flex flex-col flex-1 justify-between">
+                <div className="space-y-4">
+                  <h2 className="text-xl font-semibold text-[#05F2F2]">
+                    Digi Tools
+                  </h2>
+                  <p className="text-[#7588A3]">
+                    DigiTools is a React.js project for subscribing to digital
+                    tools like AI and content writing services. It features a
+                    smart cart system, duplicate item alerts, toast
+                    notifications, and a checkout process that clears the cart
                   </p>
                 </div>
 
