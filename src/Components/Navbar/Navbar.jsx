640adff7afbee02d5c05401fcaf6f792cd947f58
jubayer50 <aujubayer1@gmail.com>
2026-05-16 16:40:23 +0600

    link added in hire me button

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -70,9 +70,14 @@ const Navbar = () => {
 
           <div className="navbar-end">
             <div className="">
-              <button className="btn font-medium bg-[#05F2F2] border-none text-[#021525] shadow-none">
-                Hire me
-              </button>
+              <Link
+                href="https://mail.google.com/mail/?view=cm&fs=1&to=usjubayer1@gmail.com"
+                target="_blank"
+              >
+                <button className="btn font-medium bg-[#05F2F2] border-none text-[#021525] shadow-none">
+                  Hire me
+                </button>
+              </Link>
             </div>
           </div>
         </div>
