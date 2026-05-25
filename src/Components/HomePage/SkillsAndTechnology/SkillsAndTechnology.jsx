5c8e2de825d70c816ac42107a3722263c0402efb
jubayer50 <aujubayer1@gmail.com>
2026-05-25 13:51:22 +0600

    skills added

--- src/Components/HomePage/SkillsAndTechnology/SkillsAndTechnology.jsx
+++ src/Components/HomePage/SkillsAndTechnology/SkillsAndTechnology.jsx
@@ -8,6 +8,7 @@ import Express from "@/assets/Images/expressjs.png";
 import Redux from "@/assets/Images/redux.png";
 import Git from "@/assets/Images/git.png";
 import Ai from "@/assets/Images/ai.png";
+import MongoDB from "@/assets/Images/mongodb.png";
 
 const SkillsAndTechnology = () => {
   return (
@@ -195,6 +196,28 @@ const SkillsAndTechnology = () => {
 
           <div className="mt-4">
             <h3 className="font-semibold text-xl text-[#05F2F2]">Git</h3>
+            <p className="font-medium text-[#7588A3]"></p>
+          </div>
+        </div>
+
+        <div className="group relative overflow-hidden text-center border border-[#0367A650] rounded-xl p-5 bg-[#011b3075] hover:scale-103 duration-300 transform transition-all ease-in hover:border-[#05F2F290]">
+          {/* blur effect */}
+          <div>
+            <div className="h-10 w-10 rounded-full opacity-0 group-hover:opacity-100 bg-[#05F2F2] blur-3xl absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] transition-all duration-500"></div>
+          </div>
+
+          <div>
+            <Image
+              src={MongoDB}
+              alt="JavaScript"
+              width={400}
+              height={400}
+              className="w-13 mx-auto rounded-md"
+            ></Image>
+          </div>
+
+          <div className="mt-4">
+            <h3 className="font-semibold text-xl text-[#05F2F2]">MongoDB</h3>
             <p className="font-medium text-[#7588A3]"></p>
           </div>
         </div>
